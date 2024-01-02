7b830a43ad4056c0c08a80b674e01384997371a3
Ankit Dani <ankitdani1997@gmail.com>
2024-01-01 18:55:52 -0800

    Created a new config file for eslint

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
     //change base url after using ngrok on port localhost 8080
-    baseURL: 'https://7c92-2601-647-4d7f-a7d0-98b6-ca1c-4882-9381.ngrok-free.app',
+    baseURL: 'https://f718-2601-647-4d7f-a7d0-98b6-ca1c-4882-9381.ngrok-free.app',
     headers:{"ngrok-skip-browser-warning" : "true"}
 });
